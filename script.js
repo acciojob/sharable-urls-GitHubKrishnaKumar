67d6d4d65d596b9aefd245f6e99f69b593d057fb
Krishna Kumar <86869782+GitHubKrishnaKumar@users.noreply.github.com>
2024-07-15 15:11:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+document.getElementById('button').addEventListener('click', function() {
+            let name = document.getElementById('name').value;
+            let year = document.getElementById('year').value;
+
+            let url = 'https://localhost:8080/';
+            let queryString = [];
+
+            if (name) {
+                queryString.push('name=' + encodeURIComponent(name));
+            }
+            if (year) {
+                queryString.push('year=' + encodeURIComponent(year));
+            }
+
+            if (queryString.length > 0) {
+                url += '?' + queryString.join('&');
+            }
+
+            document.getElementById('url').innerText = url;
+        });
